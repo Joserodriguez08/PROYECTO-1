ba4e61af00635bfbe292f5f30c5723ee3b09ebef
joserodriguez08 <csebucaramanga1@gmail.com>
2022-04-08 22:04:13 -0500

    clase

---
+++ jugandojs.js
@@ -0,0 +1,26 @@
+console.log("hola mundo")
+// console.log(5+9)
+// console.log(5-9)
+// console.log(5*9)
+// console.log(5/9)
+
+// condicionales 
+alert("Determinar si la persona es menor de edad")
+respuesta = window.prompt("Ingresa tu edad en años", "");
+
+
+if (respuesta <=17){
+    // esto se ejecuta si la condicion es verdadera
+    alert("Eres un niño")}
+    if(respuesta >=18)
+    if(respuesta <50){ alert ("eres adulto")}
+    
+
+else {
+    // esto se ejecuta si la condicion es falsa
+    alert("Eres anciano")
+
+    console.log("esto se ejecuta si la condicion es falsa")
+}
+
+// ciclos 
